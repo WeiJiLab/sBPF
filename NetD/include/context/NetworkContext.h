7d8fd05a0125d0ad0883722ec5ea89547d9a9d51
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-25 19:41:49 +0800

    [XingfengYang ]

--- NetD/include/context/NetworkContext.h
+++ NetD/include/context/NetworkContext.h
@@ -40,7 +40,7 @@ namespace hm {
 
         public:
             std::vector<RouteData> GetRoutesData() {
-                std::lock_guard<std::mutex> lock(mutex);
+                std::lock_guard<std::mutex> lock(*mutex);
                 return this->network.routes;
             };
 
@@ -49,7 +49,7 @@ namespace hm {
         private:
             NetworkData network;
 
-            std::mutex mutex;
+            std::shared_ptr<std::mutex> mutex;
         };
     }
 }
