31af8629749a66a914eff41b31e946ab3db1d2a4
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-26 15:34:47 +0800

    [XingfengYang] #splash

--- NetD/main.cpp
+++ NetD/main.cpp
@@ -3,7 +3,12 @@
 #include "include/http_server/HttpServer.h"
 
 int main() {
-    LogInfo("NMCS 1.0 started!")
+    LogInfo(" _____ _____ _____ _____ \n"
+            "|   | |     |     |   __|\n"
+            "| | | | | | |   --|__   |\n"
+            "|_|___|_|_|_|_____|_____|\n"
+            "                         \n"
+            "       NMCS 1.0 \n")
 
     auto *netManagementCoreServer = new hm::netd::NetManagementCoreServer();
     netManagementCoreServer->Init();
