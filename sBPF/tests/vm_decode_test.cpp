428eeaabee513d2ae488516ddf16bea40271c9f2
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-01 11:00:12 +0000

    [XingfengYang] #test add tests

--- sBPF/tests/vm_decode_test.cpp
+++ sBPF/tests/vm_decode_test.cpp
@@ -648,3 +648,225 @@ TEST(VM_Decode_Test, ShouldDecodeStxdwInstructionSuccess){
  * branch instructions
  */
 
+TEST(VM_Decode_Test, ShouldDecodeJaInstructionSuccess){
+     u64 code = 0x0500004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JA);
+     ASSERT_EQ(instruction.offset,64);
+}
+
+
+TEST(VM_Decode_Test, ShouldDecodeJeqImmInstructionSuccess){
+     u64 code = 0x1550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JEQ_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJeqRegInstructionSuccess){
+     u64 code = 0x1d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JEQ_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJgtImmInstructionSuccess){
+     u64 code = 0x2550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JGT_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJgtRegInstructionSuccess){
+     u64 code = 0x2d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JGT_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJgeImmInstructionSuccess){
+     u64 code = 0x3550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JGE_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJgeRegInstructionSuccess){
+     u64 code = 0x3d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JGE_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJltImmInstructionSuccess){
+     u64 code = 0xa550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JLT_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJltRegInstructionSuccess){
+     u64 code = 0xad54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JLT_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJleImmInstructionSuccess){
+     u64 code = 0xb550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JLE_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJleRegInstructionSuccess){
+     u64 code = 0xbd54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JLE_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+
+TEST(VM_Decode_Test, ShouldDecodeJsetImmInstructionSuccess){
+     u64 code = 0x4550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSET_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsetRegInstructionSuccess){
+     u64 code = 0x4d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSET_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+
+TEST(VM_Decode_Test, ShouldDecodeJneImmInstructionSuccess){
+     u64 code = 0x5550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JNE_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJneRegInstructionSuccess){
+     u64 code = 0x5d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JNE_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsgtImmInstructionSuccess){
+     u64 code = 0x6550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSGT_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsgtRegInstructionSuccess){
+     u64 code = 0x6d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSGT_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsgeImmInstructionSuccess){
+     u64 code = 0x7550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSGE_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsgeRegInstructionSuccess){
+     u64 code = 0x7d54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSGE_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+
+TEST(VM_Decode_Test, ShouldDecodeJsltImmInstructionSuccess){
+     u64 code = 0xc550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSLT_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsltRegInstructionSuccess){
+     u64 code = 0xcd54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSLT_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsleImmInstructionSuccess){
+     u64 code = 0xd550004000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSLE_IMM);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.offset,64);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeJsleRegInstructionSuccess){
+     u64 code = 0xdd54004000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, JSLE_REG);
+     ASSERT_EQ(instruction.destRegister, 0x05);
+     ASSERT_EQ(instruction.sourceRegister,0x04);
+     ASSERT_EQ(instruction.offset, 64);
+}
+
+
+TEST(VM_Decode_Test, ShouldDecodeCallImmInstructionSuccess){
+     u64 code = 0x8500000000000040;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, CALL_IMM);
+     ASSERT_EQ(instruction.immediate, 64);
+}
+
+TEST(VM_Decode_Test, ShouldDecodeExitInstructionSuccess){
+     u64 code = 0x9500000000000000;
+     BPFInstruction_t instruction =  vm_decode_code(code);
+     ASSERT_EQ(instruction.opcode, EXIT);
+}
