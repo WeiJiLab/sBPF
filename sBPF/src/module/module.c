810e0f254b6be0d0908c6871db6cb2f43261f4fc
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-10 03:59:19 +0000

    [XingfengYang] #Release VM : macro

--- sBPF/src/module/module.c
+++ sBPF/src/module/module.c
@@ -4,38 +4,38 @@
 #include <linux/slab.h>
 
 #define ATTACH_VM(symbol)  \
-    static struct kprobe ##symbol_kp = {  \
+    static struct kprobe symbol##_kp = {  \
         .symbol_name = #symbol  \
     };  \
-    static int ##symbol_handler_pre(struct kprobe *p, struct pt_regs *regs){  \
+    static int symbol##_handler_pre(struct kprobe *p, struct pt_regs *regs){  \
         u64 program[] = {   \
          0x8500000000000001 \
         };  \
         attach_vm(regs,program,1);  \
         return 0;   \
     }   \
-    static void ##symbol_handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags){   \
+    static void symbol##_handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags){   \
     }   \
-    static int ##symbol_handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr){    \
+    static int symbol##_handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr){    \
         printk(KERN_DEBUG "fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr); \
         return 0;   \
     }
 
 #define REGISTER_KPROBE(symbol)  \
     int ret;    \
-    ##symbol_kp.pre_handler = ##symbol_handler_pre; \
-    ##symbol_kp.post_handler = ##symbol_handler_post;   \
-    ##symbol_kp.fault_handler = ##symbol_handler_fault; \
-    ret = register_kprobe(&##symbol_kp);    \
+    symbol##_kp.pre_handler = symbol##_handler_pre; \
+    symbol##_kp.post_handler = symbol##_handler_post;   \
+    symbol##_kp.fault_handler = symbol##_handler_fault; \
+    ret = register_kprobe(&symbol##_kp);    \
     if (ret < 0) {  \
         printk(KERN_DEBUG "register_kprobe failed, returned %d\n", ret);    \
         return ret; \
     }   \
-    printk(KERN_DEBUG "Planted kprobe at %p\n", ##symbol_kp.addr);
+    printk(KERN_DEBUG "Planted kprobe at %p\n", symbol##_kp.addr);
 
 #define UNREGISTER_KPROBE(symbol) \
-    unregister_kprobe(&##symbol_kp);    \
-    printk(KERN_DEBUG "kprobe at %p unregistered\n", ##symbol_kp.addr);
+    unregister_kprobe(&symbol##_kp);    \
+    printk(KERN_DEBUG "kprobe at %p unregistered\n", symbol##_kp.addr);
 
 
 
