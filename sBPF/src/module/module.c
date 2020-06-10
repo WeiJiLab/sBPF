a1b4f8410f4b705440b5076d9038cf24dda87f37
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-10 03:29:32 +0000

    [XingfengYang] #Release VM : macro

--- sBPF/src/module/module.c
+++ sBPF/src/module/module.c
@@ -3,52 +3,62 @@
 #include <linux/kprobes.h>
 #include <linux/slab.h>
 
+#define ATTACH_VM(symbol) { \
+    static struct kprobe ##symbol_kp = {  \
+        .symbol_name = #symbol  \
+    };  \
+    static int ##symbol_handler_pre(struct kprobe *p, struct pt_regs *regs){  \
+        u64 program[] = {   \
+         0x8500000000000001 \
+        };  \
+        attach_vm(regs,program,1);  \
+        return 0;   \
+    }   \
+    static void ##symbol_handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags){   \
+    }   \
+    static int ##symbol_handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr){    \
+        printk(KERN_DEBUG "fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr); \
+        return 0;   \
+    }   \
+}
+
+#define REGISTER_KPROBE(symbol) {   \
+    int ret;    \
+    ##symbol_kp.pre_handler = ##symbol_handler_pre; \
+    ##symbol_kp.post_handler = ##symbol_handler_post;   \
+    ##symbol_kp.fault_handler = ##symbol_handler_fault; \
+    ret = register_kprobe(&##symbol_kp);    \
+    if (ret < 0) {  \
+        printk(KERN_DEBUG "register_kprobe failed, returned %d\n", ret);    \
+        return ret; \
+    }   \
+    printk(KERN_DEBUG "Planted kprobe at %p\n", ##symbol_kp.addr);    \
+}
+
+#define UNREGISTER_KPROBE(symbol) {   \
+    unregister_kprobe(&##symbol_kp);    \
+    printk(KERN_DEBUG "kprobe at %p unregistered\n", ##symbol_kp.addr); \
+}
+
+
+
+
 
 extern void init_inKernel_funcction_wrapper_map(void);
 extern int attach_vm(struct pt_regs *regs, u64 program[], int size);
 
-static struct kprobe kp = {
-    .symbol_name = "sys_execve"
-};
-
-static int handler_pre(struct kprobe *p, struct pt_regs *regs){
-    u64 program[] = {
-         0x8500000000000001
-    };
-    attach_vm(regs,program,1);
-    return 0;
-}
- 
-static void handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags){
-
-}
-
-static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr){
-    printk(KERN_DEBUG "fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr);
-    return 0;
-}
+ATTACH_VM(sys_execve)
 
 static int __init kprobe_init(void){
     init_inKernel_funcction_wrapper_map();
    
-    int ret;
-    kp.pre_handler = handler_pre;
-    kp.post_handler = handler_post;
-    kp.fault_handler = handler_fault;
- 
-    ret = register_kprobe(&kp);
-    if (ret < 0) {
-        printk(KERN_DEBUG "register_kprobe failed, returned %d\n", ret);
-        return ret;
-    }
-    printk(KERN_DEBUG "Planted kprobe at %p\n", kp.addr);
+    REGISTER_KPROBE(sys_execve)
     return 0;
 }
 
 static void __exit kprobe_exit(void){
-    unregister_kprobe(&kp);
+    UNREGISTER_KPROBE(sys_execve)
     // todo: free in kernel function wrapper map
-    printk(KERN_DEBUG "kprobe at %p unregistered\n", kp.addr);
 }
 
 module_init(kprobe_init)
